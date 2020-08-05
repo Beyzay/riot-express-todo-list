bfe2f7a1d5dfdb4deabf87d725e9311085250756
beyz <b>
2020-08-04 20:20:05 -0400

    update deploy script & add shell script & yaml file & more text

--- deploy.js
+++ deploy.js
@@ -93,7 +93,7 @@ function sshConnect() {
     ssh
         .connect({
             // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-            host: '3.88.46.45',
+            host: '52.90.40.25',
             username: 'ubuntu',
             privateKey: 'riot-todo-key.pem'
         })
